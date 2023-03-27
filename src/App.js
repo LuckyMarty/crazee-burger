88dc53000eaa12cba0870f4ac2ab69298ba34982
LuckyMarty <62702759+LuckyMarty@users.noreply.github.com>
2023-03-27 11:26:59 +0200

    ✨ F02: APP > install routing system

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import ErrorPage from "./components/pages/error/ErrorPage";
 import { LoginPage } from "./components/pages/login/LoginPage";
+import OrderPage from "./components/pages/order/OrderPage";
 
 const App = () => {
   // Affichage (render)
-  return <LoginPage />
+  return (
+  <Routes>
+    <Route path="/"           element={<LoginPage />} />
+    <Route path="/order"      element={<OrderPage />} />
+    <Route path="*"           element={<ErrorPage />} />
+  </Routes>
+  )
 }
 
 export default App;
