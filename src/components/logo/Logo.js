64f940036dc513fd8c3150dc411a24a8acfea328
LuckyMarty <62702759+LuckyMarty@users.noreply.github.com>
2023-03-28 11:41:29 +0200

    ✨ F03: Add Logo, BG img

---
+++ src/components/logo/Logo.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import styled from 'styled-components'
+import { theme } from '../../theme'
+import img_logo from './assets/F03 logo-orange.png'
+
+
+export default function Logo() {
+    return (
+        <LogoStyled>
+            Crazee
+            <img src={img_logo} alt="Logo" />
+            Burger
+        </LogoStyled>
+    )
+}
+
+const font = theme.fonts;
+const color = theme.colors;
+const spacing = theme.spacing;
+
+const LogoStyled = styled.div`
+    font-family: 'Amatic SC';
+    font-size: 110px;
+    text-transform: uppercase;
+    color: ${color.primary_burger};
+    
+    >img {
+        height: 150px;
+        vertical-align: middle;
+        margin: ${spacing.md};
+    }
+`
