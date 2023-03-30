7088ecb54527023f9f7d21eaa2616cc020a7d988
LuckyMarty <62702759+LuckyMarty@users.noreply.github.com>
2023-03-30 09:32:05 +0200

    🚚 F05: ORDER > Move every Main elements in Main folder

--- src/components/pages/order/Main/Main.js
+++ src/components/pages/order/Main/Main.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { theme } from '../../../theme';
+import { theme } from '../../../../theme';
 import Basket from './Basket';
 import Menu from './Menu';
 
