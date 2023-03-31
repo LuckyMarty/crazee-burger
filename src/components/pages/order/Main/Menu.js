0ab414afcb3bd5fce1b62c4ac2702eb693f5a0b7
LuckyMarty <62702759+LuckyMarty@users.noreply.github.com>
2023-03-31 09:35:57 +0200

    💄 F06: ORDER > MENU > Responsive

--- src/components/pages/order/Main/Menu.js
+++ src/components/pages/order/Main/Menu.js
@@ -29,7 +29,20 @@ export default function Menu() {
 const MenuStyled = styled.div`
     margin: 50px auto;
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
     gap: 60px;
     justify-content: center;
+
+    grid-template-columns: repeat(4, 1fr);
+    
+    @media (max-width: 1200px) {
+        grid-template-columns: repeat(3, 1fr);
+    }
+
+    @media (max-width: 768px) {
+        grid-template-columns: repeat(2, 1fr);
+    }
+
+    @media (max-width: 640px) {
+        grid-template-columns: repeat(1, 1fr);
+    }
 `;
